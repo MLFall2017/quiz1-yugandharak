d8647114e2a31c52abbd942581bda7b28fa84dd2
Yugandhara Kulkarni <kulkarniyugandhara@gmail.com>
2017-09-22 10:19:08 -0400

    Updating PCA for quiz 1 answer

--- PCA.py
+++ PCA.py
@@ -29,7 +29,18 @@ temp = temp[::-1]
 eignevectors = eignevectors[:,temp]
 eigenvalues = eigenvalues[temp]
 answer = np.dot(inputmatrix,eignevectors)
-print answer;
+print "--------",answer,"-----";
 
 pit.plot(answer[:,0],answer[:,1],'bo')
 pit.show()
+
+'''
+Answer for given dataset = 
+[[ 0.80076073  0.52936936 -0.07064933]
+ [ 1.66940231  0.54363259 -0.00636587]
+ [ 2.61022858  0.08038336  0.0574392 ]
+ ..., 
+ [ 2.91800803  0.87258974  0.06905263]
+ [-0.31050093  0.14209605  0.07934032]
+ [ 4.08077688  0.3814206  -0.01146129]] 
+'''
